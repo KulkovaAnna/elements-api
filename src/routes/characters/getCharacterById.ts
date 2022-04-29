15b65a6a8414bdafac830aec4ce4864b568fd1ef
Anna Kulkova <anna.kulkova@interexy.com>
2022-04-29 12:58:38 +0300

    add get character by id endpoint

--- src/routes/characters/getCharacterById.ts
+++ src/routes/characters/getCharacterById.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express';
 import Character from '../../models/character.model';
 export default async function getCharacters(req: Request, res: Response) {
   // TO DO
-  const result = await Character.findAll();
+  const result = await Character.findOne({ where: { id: req.params?.id } });
   res.status(200).json(result);
 }
--- src/routes/characters/index.ts
+++ src/routes/characters/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import getCharacterById from './getCharacterById';
 import getCharacters from './getCharacters';
 const router = Router();
 
 router.get('/', getCharacters);
+router.get('/:id', getCharacterById);
 
 export default router;
