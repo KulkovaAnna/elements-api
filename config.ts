14545bd16b7c266dbb5d48946ad77659e0308e1e
Anna Kulkova <anna.kulkova@interexy.com>
2022-06-17 11:32:10 +0300

    fix: update knexfile

--- config.ts
+++ config.ts
@@ -3,18 +3,12 @@ dotenv.config({
   path: './.env',
 });
 
-const isDevEnv = process.env.NODE_ENV === 'development';
-
-export const PORT = isDevEnv ? 5000 : Number(process.env.API_PORT);
-export const DB_HOST = isDevEnv ? 'localhost' : String(process.env.DB_HOST);
+export const PORT = Number(process.env.API_PORT);
+export const DB_HOST = String(process.env.DB_HOST);
 export const DB_PORT = Number(process.env.DB_PORT);
 export const DB_NAME = String(process.env.DB_NAME);
 export const DB_USER = String(process.env.DB_USER);
 export const DB_PASSWORD = String(process.env.DB_PASSWORD);
-export const DATABASE_URL = isDevEnv
-  ? `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`
-  : String(process.env.DATABASE_URL);
-export const SRORAGE_PATH = isDevEnv
-  ? 'storage'
-  : String(process.env.STORAGE_PATH);
+export const DATABASE_URL = String(process.env.DATABASE_URL);
+export const SRORAGE_PATH = String(process.env.STORAGE_PATH);
 export const JWT_SECRET = String(process.env.JWT_SECRET);
--- knexfile.js
+++ knexfile.js
@@ -6,47 +6,33 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      port: 5432,
-      user: 'postgres',
-      password: '1111',
-      database: 'elements',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'migrations',
+    },
+  },
+
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
     },
     migrations: {
       tableName: 'migrations',
     },
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user: 'username',
-  //     password: 'password',
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //   },
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user: 'username',
-  //     password: 'password',
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //   },
-  // },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'migrations',
+    },
+  },
 };
